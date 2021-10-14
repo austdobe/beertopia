fb4b55cae69707841ca55f0da3674df6f4ddfb08
Austin <austin.dober@gmail.com>
2021-10-13 20:14:04 -0400

    adjusting Button width on mobile

--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -4,7 +4,7 @@ export const Wrapper = styled.button`
 	display: block;
 	background: var(--darkGrey);
 	width: 25%;
-	min-width: 250px;
+	min-width: 150px;
 	height: 60px;
 	border-radius: 30px;
 	color: var(--white);
